dfc99cf22f38db27ee1d209904c5322f224faa4a
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-11-27 14:13:19 +1300

    add UI embedded spotify player

--- client/Pages/MySongs/MySongs.tsx
+++ client/Pages/MySongs/MySongs.tsx
@@ -5,6 +5,15 @@ function MySongs() {
   return (
     <div>
       <h1>My Songs</h1>
+      <iframe
+        title="Here In Spirit"
+        className="p-4"
+        src="https://open.spotify.com/embed/track/6ICdz2wvVMDC4u801OwHA2?utm_source=generator"
+        width="100%"
+        height="152"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
     </div>
   )
 }
