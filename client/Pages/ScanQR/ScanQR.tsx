f89f982b94217da7d33e8b0cc669b4eab3397c85
David <14233215+davidlpoole@users.noreply.github.com>
2023-11-27 15:39:57 +1300

    Add QR code scanner component

--- client/Pages/ScanQR/ScanQR.tsx
+++ client/Pages/ScanQR/ScanQR.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+import { QrReader } from 'react-qr-reader'
+
 function ScanQR() {
-  return <div>Sup buddy</div>
+  const [data, setData] = useState('No result')
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          console.log(result)
+          if (result) {
+            setData(result?.text)
+          }
+
+          if (error) {
+            console.info(error)
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
+  )
 }
 
 export default ScanQR
