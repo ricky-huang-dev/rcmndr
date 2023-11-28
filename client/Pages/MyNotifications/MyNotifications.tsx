90caf9e58b3565fd28a3693e027c33589654bbc2
Gapson <50922615+hgapson@users.noreply.github.com>
2023-11-28 14:18:06 +1300

    add tailwind to notifications

--- client/Pages/MyNotifications/MyNotifications.tsx
+++ client/Pages/MyNotifications/MyNotifications.tsx
@@ -1,3 +1,5 @@
+import Button from '../../components/UI/Button/Button'
+import Icon from '../../components/UI/Icon/Icon'
 import useNotifications from '../../hooks/useNotifications'
 import useUpdateTitle from '../../hooks/useUpdateTitle'
 
@@ -6,10 +8,27 @@ function MyNotification() {
   const { data } = useNotifications()
   return (
     <div>
+      <h1 className="py-1 px-4 text-4xl font-bold">My notifications</h1>
+      <Button>Dismiss</Button>
       <ul>
         {data?.map((notification) => {
           return (
-            <li key={notification.notificationId}>{notification.message}</li>
+            <li
+              key={notification.notificationId}
+              className="flex list-none gap-1 py-1 px-4"
+            >
+              <div className="self-center flex-none">
+                <Icon>
+                  <i className="fa-solid fa-user" />
+                </Icon>
+              </div>
+              <div className="flex flex-col w-36 flex-auto">
+                <h3 className="text-white">{notification.message}</h3>
+                <h4 className="text-xs text-lightPurple">
+                  {notification.nickname}
+                </h4>
+              </div>
+            </li>
           )
         })}
       </ul>
